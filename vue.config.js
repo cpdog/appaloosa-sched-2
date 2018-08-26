1f6004a2666cc4ddffe831236c26c1f1623fd3aa
tim.clark <cpdog@users.noreply.github.com>
2018-08-26 09:54:59 -0400

    fixed title. excluded web.config from precache

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? '/'
-        : '/'
-}
+  baseUrl: process.env.NODE_ENV === "production" ? "/" : "/",
+  pwa: {
+    workboxOptions: {
+      exclude: [
+        "web.config",
+        "robots.txt",
+        /\.map$/,
+        /^manifest.*\.js(?:on)?$/,
+        /\/icons/
+      ]
+    }
+  }
+};
