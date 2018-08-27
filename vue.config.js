5f673af04b263ab0d212c0f553286a58d92d24d3
tim.clark <cpdog@users.noreply.github.com>
2018-08-27 14:17:23 -0400

    icons

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,13 @@ module.exports = {
         /^manifest.*\.js(?:on)?$/,
         /\/icons/
       ]
+    },
+    iconPaths: {
+      favicon32: "favicon.ico",
+      favicon16: "favicon.ico",
+      appleTouchIcon: "img/icons/icon-152x152.png",
+      maskIcon: "img/icons/icon-144x144.png",
+      msTileImage: "img/icons/icon-144x144.png"
     }
   }
 };
